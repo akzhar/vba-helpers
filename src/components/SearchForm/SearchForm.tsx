c03c073aeeca1a17c60e66e39f170846f19d1740
Akzhar <akzharik@mail.ru>
2022-07-07 12:13:54 +0300

    Add support links for search results

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/no-onchange */
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
-const MapTypeToHint: { [key: string]: string } =  {
-  KEYWORD: 'Search by keyword',
-  CATEGORY: 'Search by category'
+const SearchTypeToHint: { [key: string]: string } =  {
+  k: 'Search by keyword',
+  c: 'Search by category'
 };
 
 import ActionCreator from '@store/actions';
@@ -12,22 +13,25 @@ import ActionCreator from '@store/actions';
 const SearchForm: React.FC = () => {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
-  const [searchType, setSearchType] = useState<string>(Object.keys(MapTypeToHint)[0]);
-  const [searchHint, setSearchHint] = useState<string>(Object.values(MapTypeToHint)[0]);
+  const [searchType, setSearchType] = useState<string>(Object.keys(SearchTypeToHint)[0]);
+  const [searchHint, setSearchHint] = useState<string>(Object.values(SearchTypeToHint)[0]);
   const [searchQuery, setSearchQuery] = useState<string>('');
 
   const typeRef = useRef<HTMLSelectElement>(null);
   const queryRef = useRef<HTMLInputElement>(null);
 
+  function runSearch() {
+    dispatch(ActionCreator.setInfoMessage({ label: '😊', text: `Поиск по запросу <${searchQuery}>...` }));
+    // TODO: save searchQuery to store
+  }
+
   const formSubmitHandler = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    const value = queryRef.current?.value;
-    if(value && value === searchQuery) {
-      dispatch(ActionCreator.setInfoMessage({ label: '😊', text: `Поиск по запросу <${searchQuery}>...` }));
-      // TODO: save searchQuery to store
-      // TODO: update location url
-      queryRef.current.value = '';
+    if(searchQuery) {
+      runSearch();
     }
   };
 
@@ -36,7 +40,6 @@ const SearchForm: React.FC = () => {
     const typeValue = typeRef.current?.value;
     if(typeValue) {
       setSearchType(typeValue);
-      setSearchHint(MapTypeToHint[typeValue]);
       queryRef.current?.focus();
     }
   };
@@ -44,28 +47,46 @@ const SearchForm: React.FC = () => {
   const queryChangeHandler = (evt: React.FormEvent<HTMLInputElement>) => {
     evt.preventDefault();
     const queryValue = queryRef.current?.value;
-
     if(queryValue) {
       switch(queryValue) {
         case '@':
-          setSearchType('CATEGORY');
-          setSearchHint(MapTypeToHint.CATEGORY);
-          queryRef.current.value = '';
+          setSearchType('c');
+          setSearchQuery('');
           break;
         case '#':
-          setSearchType('KEYWORD');
-          setSearchHint(MapTypeToHint.KEYWORD);
-          queryRef.current.value = '';
+          setSearchType('k');
+          setSearchQuery('');
           break;
         default:
           setSearchQuery(queryValue);
           break;
       }
+    } else {
+      setSearchQuery('');
     }
-
   };
 
-  useEffect(() => { queryRef.current?.focus(); }, []);
+  useEffect(() => {
+    const type = searchParams.get('type');
+    const query = searchParams.get('query');
+    if(type) {
+      setSearchType(type);
+    }
+    if(query) {
+      setSearchQuery(query);
+      runSearch();
+    }
+    queryRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      navigate(`?type=${searchType}`);
+    } else {
+      navigate(`?type=${searchType}&query=${searchQuery}`);
+    }
+    setSearchHint(SearchTypeToHint[searchType]);
+  }, [searchType, searchQuery]);
 
   return (
     <form className="search" onSubmit={formSubmitHandler}>
@@ -77,13 +98,14 @@ const SearchForm: React.FC = () => {
         ref={typeRef}
         onChange={typeChangeHandler}
       >
-        { Object.keys(MapTypeToHint).map((type, i) => (<option key={i} value={type}>{type[0]}</option>)) }
+        { Object.keys(SearchTypeToHint).map((type, i) => (<option key={i} value={type}>{type.toUpperCase()}</option>)) }
       </select>
       <input
         className="search__query"
         name="query"
         type="text"
         placeholder={searchHint}
+        value={searchQuery}
         ref={queryRef}
         onChange={queryChangeHandler}
       />
