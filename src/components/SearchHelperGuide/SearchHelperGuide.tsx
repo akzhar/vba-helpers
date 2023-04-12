3afcd3ff59de4ebaca4006f66f7a8cb15f7088c9
Akzhar Taov <akzhar.taov@yandex.ru>
2023-04-12 17:40:08 +0300

    Update helper to import other helpers

--- src/components/SearchHelperGuide/SearchHelperGuide.tsx
+++ src/components/SearchHelperGuide/SearchHelperGuide.tsx
@@ -2,39 +2,41 @@ import React from 'react';
 
 import DemoImage from '@components/DemoImage';
 import Slider from '@components/Slider';
-import { DocsLinks } from '@consts/const'
+import { DocsLinks, WikiLinks } from '@consts/const'
 
-const SearchHelperGuideDemo: React.FC = () => (
+const SearchHelperGuideDemo1: React.FC = () => (
   <div>
     <p>
       There is a helper to search / import other
       helpers right from Excel <span role="img" aria-label="icon">🤯</span> How do you like it?
     </p>
-    <DemoImage url="./search-helper/demo.gif" width="64%" />
-    <p>Follow the steps to install it (to do only once)</p>
+    <p>The <b>1<small>st</small> option</b> is to call dialog popup (user form) from any worksheet:</p>
+    <DemoImage url="./search-helper/demo-form.gif" width="80%" />
+  </div>
+);
+
+const SearchHelperGuideDemo2: React.FC = () => (
+  <div>
+    <p>
+      The <b>2<small>nd</small> option</b> is to call import directly
+      from <a href={DocsLinks.IMMEDIATE_WINDOW} target="_blank" rel="noreferrer">Immediate window</a> (it&apos;s
+      something like using a <a href={WikiLinks.CLI} target="_blank" rel="noreferrer">CLI</a>):
+    </p>
+    <DemoImage url="./search-helper/demo-immediate.gif" width="80%" />
   </div>
 );
 
 const SearchHelperGuideStep1: React.FC = () => (
-  <p><a href="./search-helper/VbaHelpers.zip" download>Download zip</a> and exctract the files</p>
-);
-
-const SearchHelperGuideStep2: React.FC = () => (
   <div>
-    <p>Import all the exctracted files (<b>.bas</b> and <b>.frm</b>) into VBA project</p>
-    <p><b>Developer</b> → <b>Visual Basic</b> → <b>File</b> → <b>Import File...</b></p>
-    <DemoImage url="./search-helper/steps/import-module.png" width="34%" />
-  </div>
-);
-
-const SearchHelperGuideStep3: React.FC = () => (
-  <div>
-    <p>Move imported files (2 modules and 1 form) to <b>Personal.xlsb</b> workbook</p>
-    <DemoImage url="./search-helper/steps/move-module.png" width="34%" />
-    <p>You may have to <a href={DocsLinks.PERSONAL_XLSB} target="_blank" rel="noreferrer">create it</a> first</p>
-    <p>Just record a new macro (<b>Developer</b> → <b>Record Macro</b>) and select <b>Personal Macro Workbook</b></p>
+    <p>Follow a few simple steps to install (to do only once)</p>
+    <p>
+      To run the macro from any Excel file on your computer you have
+      to <a href={DocsLinks.PERSONAL_XLSB} target="_blank" rel="noreferrer">create Personal Macro Workbook</a>.
+      <br/>After creation it&apos;s available in any other Excel file. To create it just record any new macro:
+    </p>
+    <p><b>Developer</b> → <b>Record Macro</b> → Store macro in <b>Personal Macro Workbook</b></p>
     <DemoImage url="./search-helper/steps/create-personal-xlsb.png" width="64%" />
-    <p>Then you can find your <b>Personal.xlsb</b> in one of the following places:</p>
+    <p>Then you can find your <b>PERSONAL.XLSB</b> in one of the following places:</p>
     <ul style={{wordWrap: 'break-word'}}>
       <li><i>C:\Program Files\Microsoft Office\Office12\XLSTART</i></li>
       <li><i>C:\Documents and Settings\Computer\Application Data\Microsoft\Excel\XLSTART</i></li>
@@ -49,12 +51,35 @@ const SearchHelperGuideStep3: React.FC = () => (
   </div>
 );
 
+const SearchHelperGuideStep2: React.FC = () => (
+  <div>
+    <p>
+      To allow the macro to interact with VBA project objects you have
+      to <a href={DocsLinks.TRUST_ACCESS_VBOM} target="_blank" rel="noreferrer">enable the appropriate option</a> in
+      Excel&apos;s Trust Сenter settings
+    </p>
+    <DemoImage url="./search-helper/steps/trust-access-vbom.png" width="64%" />
+  </div>
+);
+
+
+const SearchHelperGuideStep3: React.FC = () => (
+  <div>
+    <p>
+      <a href="./search-helper/VbaHelpers.zip" download>Download zip</a> and exctract the files.
+      <br/>Import the exctracted files (<b>.bas</b> and <b>.frm</b>) into the VBAProject of <b>PERSONAL.XLSB</b>:
+    </p>
+    <p><b>Developer</b> → <b>Visual Basic</b> → Select <b>PERSONAL.XLSB</b> → <b>File</b> → <b>Import File...</b></p>
+    <DemoImage url="./search-helper/steps/import-module.png" width="34%" />
+  </div>
+);
+
 const SearchHelperGuideStep4: React.FC = () => (
   <div>
-    <p>Set shortcut to quickly call the macro right from Excel worksheet</p>
+    <p>To quickly call the macro from any Excel worksheet it&apos;s handy to set a shortcut</p>
     <p>
-      <b>Developer</b> → <b>Macros</b> → select the
-      macro → <b>Options</b> → Set shortcut (e.g. <b>Ctrl + q</b>) → <b>OK</b>
+      <b>Developer</b> → <b>Macros</b> → Select the
+      macro <b>HelpersFormOpen</b> → <b>Options</b> → Set shortcut (e.g. <b>Ctrl + q</b>) → OK
     </p>
     <DemoImage url="./search-helper/steps/set-shortcut.png" width="84%" />
   </div>
@@ -62,8 +87,11 @@ const SearchHelperGuideStep4: React.FC = () => (
 
 const SearchHelperGuideStep5: React.FC = () => (
   <div>
-    <p>Once you done, just close the file <span role="img" aria-label="icon">❌</span></p>
-    <p>You will be prompted to save changes in Personal Macro Workbook → click <b>Save</b></p>
+    <p>
+      Congrats! <span role="img" aria-label="icon">🥂</span> That&apos;s all
+      you need to do. Just close the file <span role="img" aria-label="icon">❌</span>.
+    </p>
+    <p>You will be prompted to save changes in <b>Personal Macro Workbook</b> → click <b>Save</b></p>
     <DemoImage url="./search-helper/steps/save-personal-xlsb.png" width="34%" />
   </div>
 );
@@ -71,10 +99,11 @@ const SearchHelperGuideStep5: React.FC = () => (
 const SearchHelperGuide: React.FC = () => (
   <section className="search-helper">
     <Slider items={[
-      { title: 'How it works', node: <SearchHelperGuideDemo /> },
-      { title: 'Step 1 - download', node: <SearchHelperGuideStep1 /> },
-      { title: 'Step 2 - import', node: <SearchHelperGuideStep2 /> },
-      { title: 'Step 3 - personal book', node: <SearchHelperGuideStep3 /> },
+      { title: 'How it works - form', node: <SearchHelperGuideDemo1 /> },
+      { title: 'How it works - immediate', node: <SearchHelperGuideDemo2 /> },
+      { title: 'Step 1 - personal book', node: <SearchHelperGuideStep1 /> },
+      { title: 'Step 2 - trust access', node: <SearchHelperGuideStep2 /> },
+      { title: 'Step 3 - add code', node: <SearchHelperGuideStep3 /> },
       { title: 'Step 4 - set shortcut', node: <SearchHelperGuideStep4 /> },
       { title: 'Step 5 - save', node: <SearchHelperGuideStep5 /> }
     ]} />
--- src/consts/const.ts
+++ src/consts/const.ts
@@ -10,13 +10,15 @@ export const AppRoutes: TLinks = {
 
 export const WikiLinks: TLinks = {
   VBA: 'https://en.wikipedia.org/wiki/Visual_Basic_for_Applications',
-  DRY: 'https://en.wikipedia.org/wiki/Don%27t_repeat_yourself'
+  DRY: 'https://en.wikipedia.org/wiki/Don%27t_repeat_yourself',
+  CLI: 'https://en.wikipedia.org/wiki/Command-line_interface'
 };
 
 export const DocsLinks: TLinks = {
   EXCEL_VBA_API: 'https://learn.microsoft.com/en-us/office/vba/api/overview/excel',
   PERSONAL_XLSB: 'https://support.microsoft.com/en-us/office/create-and-save-all-your-macros-in-a-single-workbook-66c97ab3-11c2-44db-b021-ae005a9bc790',
-  IMMEDIATE_WINDOW: 'https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/use-the-immediate-window'
+  IMMEDIATE_WINDOW: 'https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/use-the-immediate-window',
+  TRUST_ACCESS_VBOM: 'https://support.microsoft.com/en-gb/topic/programmatic-access-to-office-vba-project-is-denied-960d5265-6592-9400-31bc-b2ddfb94b445#ID0EDBBD'
 };
 
 export const ReposLinks: TLinks = {
